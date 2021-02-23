b6b73019dc8f108c639ebabb2c95625ea91d3c3f
ademidun <tomiademidun@gmail.com>
2021-02-23 09:43:53 -0500

    created basic web app template outline

---
+++ src/components/AccountsTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class AccountsTable extends React.Component {
+
+
+    render() {
+       
+        return(
+            <div>
+                <h1>Accounts Table</h1>
+            </div>
+        )
+    }
+}
+
+export default AccountsTable
